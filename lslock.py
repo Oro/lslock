2d1345b09d0b343c9d0b162604d9a541f16bea02
Marco Orovecchia <marco@orovecchia.at>
2016-11-12 18:33:12 +0100

    lock generation and first dummy test

--- lslock.py
+++ lslock.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+import os, fcntl, subprocess, stat
+import string, random
 import click
 
 @click.command()
@@ -6,3 +8,23 @@ import click
 def display_locked_files(path):
     """Displays all locked files underneath path."""
     click.echo(click.format_filename(path))
+
+def create_locks(directory="/tmp/lslock-test"):
+    """Creates some locked files in directory and checks the validity of lslock"""
+    pass
+
+def create_lock(directory="/tmp/lslock-test"):
+    """Creates a locked file in directory"""
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    file_name = os.path.join(directory, random_string())
+    with open(file_name, "w") as lock_file:
+        fcntl.flock(lock_file, fcntl.LOCK_NB|fcntl.LOCK_EX)
+        click.echo("Creating lock on " + click.format_filename(file_name))
+
+def random_string(size=10):
+    """Generates a random lowercase string of size"""
+    return ''.join(random.choice(string.ascii_lowercase) for x in range(size))
+
+def test():
+    assert True
--- setup.py
+++ setup.py
@@ -7,9 +7,11 @@ setup(
     py_modules=['lslock'],
     install_requires=[
         'Click',
+        'nose'
     ],
     entry_points='''
         [console_scripts]
         lslock=lslock:display_locked_files
+        lslock-test=lslock:create_locks
     ''',
 )
