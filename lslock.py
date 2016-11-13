0c3e351ff83ceaf8978193f3d8beac6e828734f5
Marco Orovecchia <marco@orovecchia.at>
2016-11-13 11:46:42 +0100

    pep8 and yapf formatting

--- lslock.py
+++ lslock.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python
-import os, fcntl, subprocess, stat
-import string, random
+import os
+import fcntl
+import string
+import random
 import click
-import time
 from multiprocessing import Process
 import logging
 
 
 @click.command()
-@click.argument('directory', type=click.Path(exists=True), default="/tmp/lslock-test")
-@click.option('--verbose', '-v', default=False, is_flag=True, help='Enable verbose output')
+@click.argument(
+    'directory', type=click.Path(exists=True), default="/tmp/lslock-test")
+@click.option(
+    '--verbose',
+    '-v',
+    default=False,
+    is_flag=True,
+    help='Enable verbose output')
 def display_locked_files(directory, verbose):
     """Displays all locked files underneath directory."""
     if verbose:
@@ -19,56 +26,76 @@ def display_locked_files(directory, verbose):
     locks = locked_files_in_dir(directory)
     logging.info('Found following locks: {}'.format(locks))
     for lock in locks:
-        click.echo('File {} is locked by PID {}'.format(click.format_filename(lock.get('filepath')), lock.get('pid')))
+        click.echo('File {} is locked by PID {}'.format(
+            click.format_filename(lock.get('filepath')), lock.get('pid')))
     if not locks:
         click.echo('Found no locked files')
 
+
 @click.command()
-@click.argument('directory', type=click.Path(exists=True), default="/tmp/lslock-test")
-@click.option('--number', '-n', default=5, help='How many lock files are generated')
-@click.option('--verbose', '-v', default=False, is_flag=True, help='Enable verbose output')
+@click.argument(
+    'directory', type=click.Path(exists=True), default="/tmp/lslock-test")
+@click.option(
+    '--number', '-n', default=5, help='How many lock files are generated')
+@click.option(
+    '--verbose',
+    '-v',
+    default=False,
+    is_flag=True,
+    help='Enable verbose output')
 def create_locks(directory, verbose, number):
-    """Creates some locked files in directory and checks the validity of lslock"""
+    """Creates some locked files in directory
+    and checks the validity of lslock"""
     if verbose:
         logging.basicConfig(level=logging.DEBUG)
 
-    for i in range(0,number):
-        full_path = os.path.join(directory,str(i))
-        p = Process(target=create_lock, args=(full_path,))
+    for i in range(0, number):
+        full_path = os.path.join(directory, str(i))
+        p = Process(target=create_lock, args=(full_path, ))
         p.start()
 
     p.join()
     click.echo("All done!")
+
 
 def create_lock(full_path):
     with open(full_path, "w") as file_to_lock:
         lock_file(file_to_lock)
         check_is_locked_file(full_path)
 
+
 def locked_files_in_dir(directory):
-    """Returns a tuple of dicts of all filepaths in directory that are currently locked"""
+    """Returns a tuple of dicts of all filepaths in directory
+    that are currently locked"""
     all_locks = {}
     locked_files = []
     files_in_directory = inodes_in_dir(directory)
     with open("/proc/locks", "r") as locks:
         for line in locks:
-            pid = line.split()[4] # The fifth position holds the pid of the process locking the file
-            inode = line.split()[5] # The sixth position which has the format MAJOR-DEVICE:MINOR-DEVICE:INODE-NUMBER
-            inode = inode.split(":")[2] # we are only interested in the inode number, so we split it on : and get the third one
+            pid = line.split()[4]  # process locking the file
+            inode = line.split()[
+                5]  # has the format MAJOR-DEVICE:MINOR-DEVICE:INODE-NUMBER
+            inode = inode.split(":")[2]  # interested only in inode number
             all_locks[int(inode)] = int(pid)
 
-    # logging.info("Here are all currently locked inodes with their PID: {}".format(all_locks))
-    # The intersection of all locks and the locks in the specified directory is what we want
+    # The intersection of all locks and the locks in the specified directory
     intersection = set(all_locks) & set(files_in_directory)
 
     logging.info("All current locks: {}".format(all_locks))
-    # to get a dict with both pid and filepath, we need to append the pid we got from /proc/locks and the associated filepath from the file inside the directory
+    # to get a dict with both pid and filepath,
+    # we need to append the pid we got from /proc/locks and the associated
+    # filepath from the file inside the directory
     for lock in intersection:
-        locked_files.append({'pid': all_locks[lock], 'filepath': files_in_directory[lock]})
+        locked_files.append({
+            'pid': all_locks[lock],
+            'filepath': files_in_directory[lock]
+        })
     return locked_files
 
+
 def inodes_in_dir(directory):
-    """Descent into directory and return a dict of all inodes with their filepath"""
+    """Descent into directory and return
+    a dict of all inodes with their filepath"""
     inode_dict = {}
     for filepath in files_in_dir(directory):
         inode_dict[os.stat(filepath).st_ino] = filepath
@@ -89,14 +116,19 @@ def files_in_dir(directory):
                 file_paths.append(filepath)
     return file_paths
 
+
 def lock_file(file_to_lock):
     """locks the passed file"""
-    fcntl.flock(file_to_lock, fcntl.LOCK_NB|fcntl.LOCK_EX)
+    fcntl.flock(file_to_lock, fcntl.LOCK_NB | fcntl.LOCK_EX)
     logging.info("Creating lock on {}".format(file_to_lock))
+
 
 def random_string(size=10):
     """Generates a random lowercase string of size"""
     return ''.join(random.choice(string.ascii_lowercase) for x in range(size))
+
+
+# Here start the tests
 
 def test_file_to_lock_creation():
     filename = random_string()
@@ -109,11 +141,14 @@ def test_file_to_lock_creation():
     logging.info("Checking if {} exists".format(full_path))
     assert os.path.isfile(full_path)
 
+
 def check_is_locked_file(full_path):
     assert is_locked(full_path)
 
+
 def check_is_unlocked_file(full_path):
     assert not is_locked(full_path)
+
 
 def is_locked(full_path):
     logging.info("Checking if {} is locked.".format(full_path))
@@ -121,29 +156,30 @@ def is_locked(full_path):
     if not locks:
         return []
     lock = [lock for lock in locks if lock.get('filepath') == full_path]
-    logging.info("Found lock on {}".format( lock))
+    logging.info("Found lock on {}".format(lock))
     return lock
+
 
 def test_five_locked_files():
     directory = "/tmp/lslock-test"
     if not os.path.exists(directory):
         os.makedirs(directory)
     for i in range(0, 5):
-        full_path = os.path.join(directory,str(i))
+        full_path = os.path.join(directory, str(i))
         with open(full_path, "w") as file_to_lock:
             lock_file(file_to_lock)
             locks = locked_files_in_dir(full_path)
             logging.info(locks)
             yield check_is_locked_file, full_path
 
+
 def test_five_unlocked_files():
     directory = "/tmp/lslock-test"
     if not os.path.exists(directory):
         os.makedirs(directory)
-    for i in range(0,5):
-        full_path = os.path.join(directory,str(i))
-        with open(full_path, "w") as unfile_to_lock:
+    for i in range(0, 5):
+        full_path = os.path.join(directory, str(i))
+        with open(full_path, "w"):
             locks = locked_files_in_dir(full_path)
             logging.info(locks)
             yield check_is_unlocked_file, full_path
-
